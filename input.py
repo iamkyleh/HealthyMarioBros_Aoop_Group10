e74a42ad670b5d84b32a198ed0d3a131d31e9054
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-19 03:04:51 +0800

    update input.py coding, support more kind of input type and is easier to maintain

--- input.py
+++ input.py
@@ -8,26 +8,35 @@ def _pressed(keys, code):
     except Exception:
         return False
 
-def get_keyboard_input_wasd(keys):
-    move_x: int = 0
-    if _pressed(keys, pygame.K_a):
+def get_keyboard_input(keys, mapping):
+    move_x = 0
+    if _pressed(keys, mapping["left"]):
         move_x -= 1
-    if _pressed(keys, pygame.K_d):
+    if _pressed(keys, mapping["right"]):
         move_x += 1
-    jump_pressed = _pressed(keys, pygame.K_w)
+    jump_pressed = _pressed(keys, mapping["jump"])
     return move_x, jump_pressed
 
-def get_keyboard_input_arrows(keys):
-    move_x: int = 0
-    if _pressed(keys, pygame.K_LEFT):
-        move_x -= 1
-    if _pressed(keys, pygame.K_RIGHT):
-        move_x += 1
-    jump_pressed = _pressed(keys, pygame.K_UP)
-    return move_x, jump_pressed
+KEYBOARD_MAPPINGS = {
+    "wasd": {
+        "left": pygame.K_a,
+        "right": pygame.K_d,
+        "jump": pygame.K_w,
+    },
+    "arrows": {
+        "left": pygame.K_LEFT,
+        "right": pygame.K_RIGHT,
+        "jump": pygame.K_UP,
+    },
+    "ijkl": {
+        "left": pygame.K_j,
+        "right": pygame.K_l,
+        "jump": pygame.K_i,
+    },
+}
 
 def input(keys, keyboard):
-    if keyboard == "wasd":
-        return get_keyboard_input_wasd(keys)
-    elif keyboard == "arrows":
-        return get_keyboard_input_arrows(keys)
+    mapping = KEYBOARD_MAPPINGS.get(keyboard)
+    if mapping:
+        return get_keyboard_input(keys, mapping)
+    return 0, False
