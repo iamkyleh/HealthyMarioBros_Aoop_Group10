b93139d753f5031071a05cfa8a1e0972a36ad338
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-18 23:54:30 +0800

    add first enemy goomba, inherit from enemy

--- enemy.py
+++ enemy.py
@@ -5,6 +5,17 @@ class Enemy(Entity):
         super().__init__(x, y, width=width, height=height)
         self.faction = 'E'
         self.vel_x: float = -1
-    def patrol(self):
+    def wander_horizonal(self):
+        pass
+
+class Goomba(Enemy):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+    def update(self, platforms):
+        if not self.is_alive:
+            return
+        self.wander_horizonal()
+        self.move_and_collide_vertical(platforms)
+    def stomped(self):
         pass
     
