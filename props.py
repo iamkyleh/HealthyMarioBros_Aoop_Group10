ec012eee4dc74cd01bda414613c32cf476c5449f
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-19 00:08:12 +0800

    add coin

--- props.py
+++ props.py
@@ -19,3 +19,28 @@ class Props():
     @abstractmethod
     def draw(self, screen, camera_x):
         pass
+
+class Coin(Props):
+    def __init__(self, x, y):
+        super().__init__(x, y, width=20, height=28)
+        self.rotation = 0.0
+        self.collected = False
+
+    def update(self):
+        if not self.collected:
+            self.rotation += 0.2
+
+    def draw(self, screen, camera_x):
+        if self.collected:
+            return
+
+        # spin illusion using sine scaling on X-axis
+        scale = abs(math.sin(self.rotation))
+        scaled_w = max(2, int(self.width * scale))  # never go to 0 width
+        scaled_image = pygame.transform.scale(self.image, (scaled_w, self.height))
+
+        # center it where the coin’s middle should be
+        x = int(self.x - camera_x) + (self.width - scaled_w) // 2
+        y = int(self.y)
+
+        screen.blit(scaled_image, (x, y))
