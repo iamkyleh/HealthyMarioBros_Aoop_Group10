e9df0ad8b5f387c72940db35d171e8c579e899df
iamkyleh <kylehsu0803@gmail.com>
2025-12-01 21:54:55 +0800

    add temp to test

---
+++ Game_Detection_iIntergrate_Test_V1/temp.py
@@ -0,0 +1,116 @@
+# pose_camera.py
+import cv2
+import mediapipe as mp
+
+class PoseCamera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_pose = mp.solutions.pose
+
+        self.pose = self.mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+    def get_frame_and_y(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None, None
+
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.pose.process(image_rgb)
+
+        hip_y_left , hip_y_right = None
+        left_shoulder , right_shoulder = None
+        left_elbow , right_elbow = None
+        
+
+        if result.pose_landmarks: 
+            # Collect the data for detecting the jumping
+            hip_y_left = result.pose_landmarks.landmark[
+                self.mp_pose.PoseLandmark.LEFT_HIP.value
+            ].y
+            
+            hip_y_right = result.pose_landmarks.landmark[
+                self.mp_pose.PoseLandmark.LEFT_HIP.value
+            ].y
+
+            self.mp_draw.draw_landmarks(
+                frame,
+                result.pose_landmarks,
+                self.mp_pose.POSE_CONNECTIONS
+            )
+
+        return frame, hip_y_left , hip_y_right
+
+    def release(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+# pose_camera.py
+import cv2
+import mediapipe as mp
+
+class PoseCamera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_pose = mp.solutions.pose
+
+        self.pose = self.mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+    def get_frame_and_y(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None, None
+
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.pose.process(image_rgb)
+
+        hip_y_left , hip_y_right = None
+        left_shoulder , right_shoulder = None
+        left_elbow , right_elbow = None
+        
+
+        if result.pose_landmarks: 
+            # Collect the data for detecting the jumping
+            hip_y_left = result.pose_landmarks.landmark[
+                self.mp_pose.PoseLandmark.LEFT_HIP.value
+            ].y
+            
+            hip_y_right = result.pose_landmarks.landmark[
+                self.mp_pose.PoseLandmark.LEFT_HIP.value
+            ].y
+
+            self.mp_draw.draw_landmarks(
+                frame,
+                result.pose_landmarks,
+                self.mp_pose.POSE_CONNECTIONS
+            )
+
+        return frame, hip_y_left , hip_y_right
+
+    def release(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
